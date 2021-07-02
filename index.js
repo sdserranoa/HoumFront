e05391a1aec05393f32e355fecaf9e213a07dc5d
Santiago Serrano <santy.zeta@hotmail.com>
2021-07-02 10:18:17 -0500

    Server

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 
-
 var app = express();
 
-app.use('/', express.static(path.join(__dirname, 'front/build')));
+const publicPath = path.join(__dirname, 'front/build');
 
-app.use(function (req, res, next) {
-    next(createError(404));
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(80);
+app.listen(80, ()=>{
+    console.log('Server is up')
+});
 
 module.exports = app;
